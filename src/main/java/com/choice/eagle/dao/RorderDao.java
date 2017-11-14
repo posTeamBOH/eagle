ef1b121440299524b1ca24eac1f51941437087ee
CodeSeven7 <1626118748@qq.com>
2017-11-14 12:31:34 +0800

    Map

--- src/main/java/com/choice/eagle/dao/RorderDao.java
+++ src/main/java/com/choice/eagle/dao/RorderDao.java
@@ -1,11 +1,11 @@
 package com.choice.eagle.dao;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public interface RorderDao {
 	
 	//根据订单号查询订单的菜品及数量
-	Map<String, Integer> selectMenuByOrderId(String orderId); 
+	HashMap<String, Integer> selectMenuByOrderId(String orderId); 
     
 	//改变订单联系表中的菜品状态
 	void updateMenuStatus(String rorderId);
