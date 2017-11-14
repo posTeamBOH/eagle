46384b349e083840ec8fa23dbd84d535e8525a2b
QiuFFF <32830599+QiuFFF@users.noreply.github.com>
2017-11-14 11:13:41 +0800

    dao测试类
    dao层所有测试类

---
+++ src/test/javaweb/com/choice/eagle/dao/TestCuisineDao.java
@@ -0,0 +1,7 @@
+package com.choice.eagle.dao;
+
+import com.choice.eagle.BaseTest;
+
+public class TestCuisineDao extends BaseTest {
+
+}
--- src/test/javaweb/com/choice/eagle/dao/TestOrderDao.java
+++ src/test/javaweb/com/choice/eagle/dao/TestOrderDao.java
@@ -1,5 +1,22 @@
 package com.choice.eagle.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.choice.eagle.entity.Order;
+
 public class TestOrderDao {
 
+	@Autowired
+	private OrderDao orderDao;
+	
+	@Test
+	public void testselectByTable() {
+		List<Order> list = orderDao.selectByTable("T1001");
+		assertEquals(1, list.size());
+	}
 }
