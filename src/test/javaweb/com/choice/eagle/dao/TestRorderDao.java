eeb5db27137f30365d1bf785d9cf48cf8ba332d6
QiuFFF <32830599+QiuFFF@users.noreply.github.com>
2017-11-14 15:02:40 +0800

    测试map

--- src/test/javaweb/com/choice/eagle/dao/TestRorderDao.java
+++ src/test/javaweb/com/choice/eagle/dao/TestRorderDao.java
@@ -3,6 +3,7 @@ package com.choice.eagle.dao;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,11 @@ public class TestRorderDao extends BaseTest {
 	@Test
 	public void testupdateMenuStatus() {
 		assertEquals(1, rorderDao.updateMenuStatus("r1001"));
+	}
+	
+	@Test
+	public void testselectMenuByOrderId() {
+		Map<String, Integer> map = rorderDao.selectMenuByOrderId("O1001");
+		assertEquals(Integer.valueOf(1), map.get("count"));
 	}
 }
