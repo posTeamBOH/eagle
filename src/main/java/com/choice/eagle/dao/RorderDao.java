15c05aa32e464027db517f1fc86043b9bab28218
QiuFFF <32830599+QiuFFF@users.noreply.github.com>
2017-11-16 13:27:54 +0800

    修改

--- src/main/java/com/choice/eagle/dao/RorderDao.java
+++ src/main/java/com/choice/eagle/dao/RorderDao.java
@@ -2,6 +2,8 @@ package com.choice.eagle.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.choice.eagle.entity.Menu;
 import com.choice.eagle.entity.MenuNum;
 
@@ -14,7 +16,7 @@ public interface RorderDao {
 	int countAllMenuByOrderId(String orderId);
     
 	//点击上菜改变订单联系表中的菜品状态
-	int updateMenuStatus(String orderId,String menuName);
+	int updateMenuStatus(@Param("orderId") String orderId,@Param("menuName") String menuName);
 	
 	//点击结账改变桌子，订单，订单联系表中的状态
 	int updateAllStatus(String tablesId,String orderId);
